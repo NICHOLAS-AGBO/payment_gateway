b7e82405da966d31c6b84d0d9feb3052da4e76b2
NICHOLAS AGBO <ndcconcept@gmail.com>
2024-04-23 16:24:07 +0100

    #Currency selection

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -2,24 +2,36 @@ import {createFileRoute} from '@tanstack/react-router'
 import {closePaymentModal, useFlutterwave} from 'flutterwave-react-v3';
 import {FlutterwaveConfig, FlutterWaveResponse} from "flutterwave-react-v3/dist/types";
 import {BaseSyntheticEvent, useMemo, useState} from "react";
-import {Button, IconButton, OutlinedInput, Stack, Typography} from "@mui/material";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select, SelectChangeEvent,
+  Stack,
+  Typography
+} from "@mui/material";
 import {
   AccountCircleOutlined, ArrowRightAltOutlined,
-  CurrencyExchangeOutlined,
   EmailOutlined,
   PhoneOutlined
 } from "@mui/icons-material";
 import cryptoRandomString from "crypto-random-string";
+import {Helmet} from "react-helmet";
 
 export const Route = createFileRoute('/')({
   component: Index
-})
+});
+
 interface Config extends FlutterwaveConfig{}
 interface Data {
   name: string,
   email: string,
   amount: number,
-  phone: string
+  phone: string,
+  currency: string
 }
 
 function Index(){
@@ -28,15 +40,16 @@ function Index(){
     name: '',
     email: '',
     phone: '',
-    amount: 0
+    amount: 0,
+    currency: 'USD'
   });
-
+  const CURRENCY = ['AUD','BRL','CAD','CNY','EGP','ETB','EUR','GBP','GHS','INR','JPY','KES','MAD','MUR','MWK','MYR','NGN','RWF','TZS','UGX','USD','XAF','XOF','ZAR','ZMW','ENGN'];
   const config = useMemo<Config>(()=>({
     public_key: import.meta.env.VITE_FLW_PUBLIC_KEY,
     tx_ref: ref,
     amount: data.amount,
     redirect_url: "https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg",
-    currency: "NGN",
+    currency: data.currency,
     payment_options: 'card,mobilemoney,ussd',
     customer: {
       email: data.email,
@@ -51,7 +64,7 @@ function Index(){
   }),[data.amount, data.email, data.name, data.phone, ref]);
 
   const handleFlutterPayment = useFlutterwave(config);
-  const getVal = ({target}:BaseSyntheticEvent)=>{
+  const getVal = ({target}:BaseSyntheticEvent|SelectChangeEvent)=>{
     setData(prevState => ({...prevState, [target.name]: target.value}));
     if (target.name === "amount"){
       target.setAttribute("min",'0');
@@ -63,6 +76,14 @@ function Index(){
              mx={"auto"} p={3} maxWidth={400} width={"100%"}
              sx={{bgcolor: "common.white"}}
       >
+        <Helmet>
+          <style>{
+            `.TanStackRouterDevtools{
+            display: none;
+            }`
+          }
+          </style>
+        </Helmet>
         <Typography variant={"h6"} align={"center"}>Enter payment details</Typography>
          <OutlinedInput
              id={"name"}
@@ -110,7 +131,24 @@ function Index(){
             required
             fullWidth
             placeholder={"0.00"}
-            endAdornment={<IconButton><CurrencyExchangeOutlined sx={{color: "primary.main"}}/></IconButton>}
+            endAdornment={
+              <FormControl variant="standard" sx={{ mr: -1, my: 1, ml: 1, width: 120 }}>
+                <InputLabel id="currency">currency</InputLabel>
+                <Select
+                    labelId="currency"
+                    id="currency"
+                    value={data.currency}
+                    label="currency"
+                    name="currency"
+                    onChange={getVal}
+                    variant={"filled"}
+                >
+                  {CURRENCY.map((val, i)=>(
+                      <MenuItem key={i} value={val}>{val}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            }
         />
         <Button  onClick={() => {
           handleFlutterPayment({
@@ -129,3 +167,75 @@ function Index(){
       </Stack>
   );
 }
+
+
+/*
+
+<FormControl fullWidth>
+  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={age}
+    label="Age"
+    onChange={handleChange}
+  >
+    <MenuItem value={10}>Ten</MenuItem>
+    <MenuItem value={20}>Twenty</MenuItem>
+    <MenuItem value={30}>Thirty</MenuItem>
+  </Select>
+</FormControl>
+
+<select class="select__input select__input--fs-14 select__input--fcase-upper" data-option-values="true"><option value="AUD">
+
+                </option><option value="BRL">
+
+                </option><option value="CAD">
+
+                </option><option value="CNY">
+
+                </option><option value="EGP">
+
+                </option><option value="ETB">
+
+                </option><option value="EUR">
+
+                </option><option value="GBP">
+
+                </option><option value="GHS">
+
+                </option><option value="INR">
+
+                </option><option value="JPY">
+
+                </option><option value="KES">
+
+                </option><option value="MAD">
+
+                </option><option value="MUR">
+
+                </option><option value="MWK">
+
+                </option><option value="MYR">
+
+                </option><option selected="selected" value="NGN">
+
+                </option><option value="RWF">
+
+                </option><option value="TZS">
+
+                </option><option value="UGX">
+
+                </option><option value="USD">
+
+                </option><option value="XAF">
+
+                </option><option value="XOF">
+
+                </option><option value="ZAR">
+
+                </option><option value="ZMW">
+
+                </option><option value="ENGN">
+
+                </option></select>*/
