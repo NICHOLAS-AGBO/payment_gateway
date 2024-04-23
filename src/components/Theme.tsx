47b006adda2532d1197d12c86d83a43c3be60eae
NICHOLAS AGBO <ndcconcept@gmail.com>
2024-04-23 17:03:11 +0100

    Update Theme.tsx

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -14,7 +14,7 @@ export default function Theme({children}:{children: ReactNode}) {
 
         <ThemeProvider theme={theme}>
             <CssBaseline/>
-            <Stack minHeight={"100vh"} sx={{backgroundColor: "#222222", px: 1.5}}
+            <Stack minHeight={"100vh"} sx={{px: 1.5}}
                    justifyContent={"center"} alignItems={"center"}>
                 {children}
             </Stack>
